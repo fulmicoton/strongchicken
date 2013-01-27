7b2c61f8f73cdd3dc784d55cf7be20f2b31d8892
Paul Masurel <paul.masurel@gmail.com>
2013-01-27 11:41:02 +0100

    Have example display spot traj if pylab is available.

--- examples/plot_market1f.py
+++ examples/plot_market1f.py
@@ -11,17 +11,24 @@ from strongchicken.uncertainties import *
 from strongchicken.core import *
 from strongchicken.utils import *
 
-#from pylab import plot, show
-
 timeline = np.arange(0., UNIT.YEAR, UNIT.DAY)
 F0 = RegularTimeSerie(0., UNIT.DAY, 50.+10.*np.sin(INV .WEEK*timeline) )
 market = Market1F(F0, .0*INV.YEAR, 0.2 * INV.SQRT.YEAR, INV.WEEK)
 
 simulator = ForwardSimulator(UncertaintySystem([market]), timeline, 10)
-for i in range(10):
-	forward_curves = simulator.state.cursor(market)
-	print forward_curves.spot()
-	simulator.go_next()
+forward_curves = simulator.state.cursor(market)
 
-#plot(F0(timeline)) 
-#show()
+spot_trajectories = np.zeros((360, 10))
+
+for i in range(360):   
+    spot_trajectories[i,:] = forward_curves.spot()[0]
+    simulator.go_next()
+
+try:
+    from pylab import plot, show
+    plot(spot_trajectories) 
+    show()
+except:
+    print "You need pylab to display graphs."
+    print spot_trajectories
+
